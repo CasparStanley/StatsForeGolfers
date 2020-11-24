09ade6235e51ad0f1f008df684bcbfafc09ac0fb
Caspar Stanley With <casparstan+creative@gmail.com>
2020-11-24 12:31:55 +0100

    Slettet alt det der lort

--- StatsForeGolfers/SFGproto/Program.cs
+++ StatsForeGolfers/SFGproto/Program.cs
@@ -7,18 +7,7 @@ namespace SFGproto
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-// <<<<<<< HEAD
-            Console.WriteLine("IT is me GAI");
-// =======
-// JEG HEDDER CASPAR.
-// >>>>>>> cb5a50ec1312dd5e51c4a64ad08ec42653e7dbf7
-            Console.WriteLine("According to the laws of physics, it shouldn't be physically possible for a bee to fly");
-<<<<<<< HEAD
-            Console.WriteLine("Somebody once told me, hands off my macaroni. He didn't and now he's shot dead");
-=======
-
-// NEW CONFLICT TEST WITG PATRICK
->>>>>>> 84f04b362bb711e121d8644095f9483e7a2015d2
+            // SLETTET ALT DET DER LORT
         }
     }
 }
