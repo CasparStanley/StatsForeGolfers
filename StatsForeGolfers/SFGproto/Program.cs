c3d5d2c800440f2dfa633077a729de91b845902a
Patrick Nyskjold <patr447g@edu.easj.dk>
2020-11-24 12:33:25 +0100

    Fix

--- StatsForeGolfers/SFGproto/Program.cs
+++ StatsForeGolfers/SFGproto/Program.cs
@@ -13,12 +13,12 @@ namespace SFGproto
 // JEG HEDDER CASPAR.
 // >>>>>>> cb5a50ec1312dd5e51c4a64ad08ec42653e7dbf7
             Console.WriteLine("According to the laws of physics, it shouldn't be physically possible for a bee to fly");
-<<<<<<< HEAD
+// <<<<<<< HEAD
             Console.WriteLine("Somebody once told me, hands off my macaroni. He didn't and now he's shot dead");
-=======
+// =======
 
 // NEW CONFLICT TEST WITG PATRICK
->>>>>>> 84f04b362bb711e121d8644095f9483e7a2015d2
+// >>>>>>> 84f04b362bb711e121d8644095f9483e7a2015d2
         }
     }
 }
