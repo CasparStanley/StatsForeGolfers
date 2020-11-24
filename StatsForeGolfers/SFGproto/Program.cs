3f18369bd4b611e1a8fbe85074e4554032f6c655
KOLGUJEV\Michael <mich5m602@edu.easj.dk>
2020-11-24 12:00:36 +0100

    Created Initial project

---
+++ StatsForeGolfers/SFGproto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SFGproto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
