6efd297ce44b19aeb4a38d91e16d23f0ea31b63a
Patrick Nyskjold <patr447g@edu.easj.dk>
2020-11-24 12:23:46 +0100

    All Stars - testing

--- StatsForeGolfers/SFGproto/Program.cs
+++ StatsForeGolfers/SFGproto/Program.cs
@@ -13,6 +13,7 @@ namespace SFGproto
 // JEG HEDDER CASPAR.
 // >>>>>>> cb5a50ec1312dd5e51c4a64ad08ec42653e7dbf7
             Console.WriteLine("According to the laws of physics, it shouldn't be physically possible for a bee to fly");
+            Console.WriteLine("Somebody once told me, hands off my macaroni. He didn't and now he's shot dead");
         }
     }
 }
