a36049206d334dff768bab30b539d7dfce4e042e
Caspar Stanley With <casparstan+creative@gmail.com>
2020-11-24 12:36:40 +0100

    S L E T

--- StatsForeGolfers/SFGproto/Program.cs
+++ StatsForeGolfers/SFGproto/Program.cs
@@ -7,22 +7,8 @@ namespace SFGproto
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-<<<<<<< HEAD
-// <<<<<<< HEAD
-            Console.WriteLine("IT is me GAI");
-// =======
-// JEG HEDDER CASPAR.
-// >>>>>>> cb5a50ec1312dd5e51c4a64ad08ec42653e7dbf7
-            Console.WriteLine("According to the law of physics, it shouldn't be physically possible for a bee to fly");
-// <<<<<<< HEAD
-            Console.WriteLine("Somebody once told me, hands off my macaroni. He didn't and now he's shot dead");
-// =======
 
-// NEW CONFLICT TEST WITG PATRICK
-// >>>>>>> 84f04b362bb711e121d8644095f9483e7a2015d2
-=======
             // SLETTET ALT DET DER LORT
->>>>>>> 09ade6235e51ad0f1f008df684bcbfafc09ac0fb
         }
     }
 }
