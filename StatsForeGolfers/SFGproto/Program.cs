84f04b362bb711e121d8644095f9483e7a2015d2
Caspar Stanley With <casparstan+creative@gmail.com>
2020-11-24 12:23:39 +0100

    Test with patrick

--- StatsForeGolfers/SFGproto/Program.cs
+++ StatsForeGolfers/SFGproto/Program.cs
@@ -13,6 +13,8 @@ namespace SFGproto
 // JEG HEDDER CASPAR.
 // >>>>>>> cb5a50ec1312dd5e51c4a64ad08ec42653e7dbf7
             Console.WriteLine("According to the laws of physics, it shouldn't be physically possible for a bee to fly");
+
+// NEW CONFLICT TEST WITG PATRICK
         }
     }
 }
