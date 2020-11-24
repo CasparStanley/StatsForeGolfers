00d69498ba3ff9669ecad52c535b64df9e427df3
Jacob Pingel Lauridsen <jaco8911@edu.easj.dk>
2020-11-24 12:05:38 +0100

    Test

--- StatsForeGolfers/SFGproto/Program.cs
+++ StatsForeGolfers/SFGproto/Program.cs
@@ -7,6 +7,7 @@ namespace SFGproto
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Bob, And I'm a pretty good golfer, I think.... ");
         }
     }
 }
