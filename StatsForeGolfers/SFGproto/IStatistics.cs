adad99511e436223a4e02d37b32b0ac993fd4c50
Caspar Stanley With <casparstan+creative@gmail.com>
2020-11-26 13:02:50 +0100

    IStatistics Interface

---
+++ StatsForeGolfers/SFGproto/IStatistics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFGproto
+{
+    interface IStatistics
+    {
+        public double GreenHits(double hits);
+        public double GreenMiss(double hits);
+        public double MissLeft();
+        public double MissRight();
+    }
+}
