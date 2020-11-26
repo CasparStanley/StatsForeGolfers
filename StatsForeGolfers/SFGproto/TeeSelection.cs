0099e8a2e762f992f9c9233a3728a450530b3d43
Caspar Stanley With <casparstan+creative@gmail.com>
2020-11-26 12:33:46 +0100

    Some properties for TeeSelection and User

--- StatsForeGolfers/SFGproto/TeeSelection.cs
+++ StatsForeGolfers/SFGproto/TeeSelection.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SFGproto
+class TeeSelection
 {
-    class TeeSelection
-    {
-    }
+    /// <summary>
+    /// Each hole has a different "Tee", which is the length of the Hole, 
+    /// and by extension, the difficulty
+    /// Tee's can be a color, or just the actual length of the specific track.
+    /// </summary>
+  
+    public string Name { get; set; }
+    public int TotalLength { get; set; }
+
+
 }
--- StatsForeGolfers/SFGproto/User.cs
+++ StatsForeGolfers/SFGproto/User.cs
@@ -6,5 +6,9 @@ namespace SFGproto
 {
     class User
     {
+        // The name of the user
+        public string Name { get; set; }
+        // The "status" of the user - Pro, Amateur, N00b, Senior, etc.
+        public string Status { get; set; }
     }
 }
