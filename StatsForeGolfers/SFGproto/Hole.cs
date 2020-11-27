c41a5b5602a60c273bf61251c8482bdb2011880f
tobi4504 <72128467+tobi4504@users.noreply.github.com>
2020-11-27 12:28:30 +0100

    Pulled out Methods from Hole and Inserted into a new class called StatSheet

--- StatsForeGolfers/SFGproto/Hole.cs
+++ StatsForeGolfers/SFGproto/Hole.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace SFGproto
 {
-    class Hole : IStatistics
+    class Hole 
     {
         /// <summary>
         /// A Course contains 18 Holes which can contain different kind of Par(The amount of shots you have) 
@@ -14,16 +14,6 @@ namespace SFGproto
         public int Par { get; set; }
         public int Length { get; set; }
         public int Handicap { get; set; }
-        public  int TotalHits { get; set; }
-        public bool GreenHit { get; set; }     
-        public bool GreenMissRight { get; set; }
-        public bool GreenMissLeft { get; set; }
-        // If green miss---> did it hit Bunker, rough or French
-        public bool Bunker { get; set; }
-        public bool Rough { get; set; }
-        public bool French { get; set; }
-        public bool Scramble { get; set; }
-        public int TotalMisses { get; set; }
 
         public Hole()
         {
@@ -35,54 +25,6 @@ namespace SFGproto
             Length = length;
             Handicap = handicap;
         }
-        public double GreenHits(int hits)
-        {
-            if (TotalHits != 0 && hits != 0)
-            {
-                hits = (TotalHits / hits);
-            }
-            return hits;
-        }
-
-        // Method Section
-        public double GreenMisses(int misses)
-        {
-            TotalMisses += misses;
-            if (TotalHits != 0 && misses != 0)
-            {
-                misses = (misses / TotalHits);
-            }
-            return misses;
-        }
-
-        public double GreenMissesLeft(int misses)
-        {
-            TotalMisses += misses;
-            if (TotalHits != 0 && misses != 0)
-            {
-                misses = (misses / TotalHits);
-            }
-            return misses;
-        }
-
-        public double GreenMissesRight(int misses)
-        {
-            TotalMisses += misses;
-            if (TotalHits != 0 && misses != 0)
-            {
-                misses = (misses / TotalHits);
-            }
-            return misses;
-        }
-
-        public double ScrambleIn(int scrambleIn)
-        {
-            if (TotalMisses != 0 && TotalMisses <= 18)
-            {
-                scrambleIn = scrambleIn / TotalMisses;
-            }
-            return scrambleIn;
-        }
-
+       
     }
 }
---
+++ StatsForeGolfers/SFGproto/StatSheet.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFGproto
+{
+    class StatSheet : IStatistics
+    {
+        public int TotalHits { get; set; }
+        public int TotalGreenHits { get; set; }
+        public bool GreenHit { get; set; }
+        public bool GreenMissRight { get; set; }
+        public bool GreenMissLeft { get; set; }
+        // If green miss---> did it hit Bunker, rough or French
+        public bool Bunker { get; set; }
+        public bool Rough { get; set; }
+        public bool French { get; set; }
+        public int FairWayHit { get; set; }
+        public bool FairWayMissLeft { get; set; }
+        public bool FairWayMissRight { get; set; }
+        public bool Scramble { get; set; }
+        public int TotalMisses { get; set; }
+
+        public double FairWayHits(int hits,int totalFairWayHits)
+        {
+            FairWayHit = hits;
+            if (totalFairWayHits != 0 && hits != 0)
+            {
+                hits = (totalFairWayHits / hits);
+            }
+            return hits;
+        }
+        public double FairWayMisses(int misses,int totalFairWayHits)
+        {
+             
+            if (totalFairWayHits != 0 && misses != 0)
+            {
+                misses = (misses / totalFairWayHits);
+            }
+            return misses;
+        }
+
+        public double FairWayMissesLeft(int misses,int totalFairWayHits)
+        {
+            
+            if (totalFairWayHits != 0 && misses != 0)
+            {
+                misses = (misses / TotalHits);
+            }
+            return misses;
+        }
+
+        public double FairWayMissesRight(int misses, int totalFairWayHits)
+        {
+            if (totalFairWayHits != 0 && misses != 0)
+            {
+                misses = (misses / totalFairWayHits);
+            }
+            return misses;
+        }
+
+        public double GreenHits(int hits, int totalGreenHits)
+        {
+            if (totalGreenHits != 0 && hits != 0)
+            {
+                hits = (totalGreenHits / hits);
+            }
+            return hits;
+        }
+        public double GreenMisses(int misses, int totalGreenHits)
+        {
+           
+            if (totalGreenHits != 0 && misses != 0)
+            {
+                misses = (misses / totalGreenHits);
+            }
+            return misses;
+        }
+
+        public double GreenMissesLeft(int misses, int totalGreenHits)
+        {
+            
+            if (totalGreenHits != 0 && misses != 0)
+            {
+                misses = (misses / totalGreenHits);
+            }
+            return misses;
+        }
+
+        public double GreenMissesRight(int misses, int totalGreenHits)
+        {
+            if (totalGreenHits != 0 && misses != 0)
+            {
+                misses = (misses / totalGreenHits);
+            }
+            return misses;
+        }
+
+        public double ScrambleIn(int scrambleIn,int totalMisses)
+        {
+            if (totalMisses != 0 && totalMisses <= 18)
+            {
+                scrambleIn = scrambleIn / tot;
+            }
+            return scrambleIn;
+        }
+
+    }
+}
