f40c73823fe83c0c9612684d990b27b203f29434
tobi4504 <72128467+tobi4504@users.noreply.github.com>
2020-12-02 12:10:38 +0100

    getting their

--- StatsForeGolfers/ProtoWeb/Pages/Courses/GetAllCourses.cshtml.cs
+++ StatsForeGolfers/ProtoWeb/Pages/Courses/GetAllCourses.cshtml.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProtoWeb.Interfaces;
+using ProtoWeb.Models;
 
-namespace ProtoWeb.Pages.Courses
+namespace ProtoWeb.Pages.Holes
 {
     public class GetAllCoursesModel : PageModel
     {
-        public void OnGet()
+        private ICourses courses;
+
+        public GetAllCoursesModel(ICourses repository)
         {
+            course = repository;
+        }
+        public Dictionary<int, Course> Courses { get; private set; }
+        [BindProperty(SupportsGet = true)]
+        public string FilterCriteria { get; set; }
+        public IActionResult OnGet()
+        {
+            Courses = courses.AllCourses();
+            if (!string.IsNullOrEmpty(FilterCriteria))
+            {
+                Courses = courses.FilterCourse(FilterCriteria);
+            }
+            return Page();
         }
     }
 }
+
