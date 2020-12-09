e238bb315b3f08df7c23e2927ff1006613715de3
Jacob Pingel Lauridsen <jaco8911@edu.easj.dk>
2020-12-09 21:02:52 +0100

    Footwork on ProStats - mangler dog noget mere arbejde!

---
+++ StatsForeGolfers/ProtoWeb/Models/ProStatsRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProtoWeb.Pages.ProStats;
+
+namespace ProtoWeb.Models
+{
+    
+    public class ProStatsRepository
+    {
+        private List<ProStatisticsModel> PSAverages { get; }
+
+        public ProStatsRepository()
+        {
+            PSAverages = new List<ProStatisticsModel>();
+            PSAverages.Add(new ProStatisticsModel(){ScoringAverage = 71.3, DrivingDistance = 295.4, FairwayAverage = 58.9, GreenInRegAverage = 66.6, ScramblingAverage = 57.1});
+        }
+
+        public IEnumerable<ProStatisticsModel> GetAllPSAverages()
+        {
+            return PSAverages.ToList();
+        }
+    }
+}
--- StatsForeGolfers/ProtoWeb/Pages/ProStats/ProStatistics.cshtml.cs
+++ StatsForeGolfers/ProtoWeb/Pages/ProStats/ProStatistics.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProtoWeb.Models;
 
 namespace ProtoWeb.Pages.ProStats
 {
@@ -14,6 +15,7 @@ namespace ProtoWeb.Pages.ProStats
         public double FairwayAverage { get; set; }
         public double GreenInRegAverage { get; set; }
         public double ScramblingAverage { get; set; }
+        
 
         // Should not be using a constructor
         //public ProStatisticsModel(double scoringAverage, double drivingDistance, double fairwayAverage, double greenInRegAverage, double scramblingAverage)
@@ -30,9 +32,17 @@ namespace ProtoWeb.Pages.ProStats
             return $"{nameof(ScoringAverage)}: {ScoringAverage}, {nameof(DrivingDistance)}: {DrivingDistance}, {nameof(FairwayAverage)}: {FairwayAverage}, {nameof(GreenInRegAverage)}: {GreenInRegAverage}, {nameof(ScramblingAverage)}: {ScramblingAverage}";
         }
 
+        private ProStatsRepository repo;
+        public List<ProStatisticsModel> PSAverages { get; private set; }
+
+        public ProStatisticsModel()
+        {
+            repo = new ProStatsRepository();
+        }
+        
         public void OnGet()
         {
-
+            PSAverages = (List<ProStatisticsModel>)repo.GetAllPSAverages();
         }
     }
 }
