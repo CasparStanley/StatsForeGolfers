0d39a6e6bcfbee92f8dd360f73e0b2e500de64c6
Caspar Stanley With <casparstan+creative@gmail.com>
2020-12-10 15:58:56 +0100

    More InputStats

--- StatsForeGolfers/ProtoWeb/Pages/Statistics/InputStats3.cshtml.cs
+++ StatsForeGolfers/ProtoWeb/Pages/Statistics/InputStats3.cshtml.cs
@@ -4,22 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProtoWeb.Interfaces;
+using ProtoWeb.Models;
 
 namespace ProtoWeb.Pages.Statistics
 {
     public class InputStats3Model : PageModel
     {
+        private IStatistics statistics;
+        private ICourses courses;
+
         [BindProperty]
-        public StatSheet StatSheet { get; set; }
+        public StatSheet MockSheet { get; private set; }
+
+        public InputStats3Model(IStatistics statsRepo, ICourses courseRepo)
+        {
+            statistics = statsRepo;
+            courses = courseRepo;
+
+            MockSheet = statistics.GetSheet();
+        }
+
+        public Course CurrentCourse { get; private set; }
+        public Dictionary<int, Hole> Holes { get; private set; }
+
         public IActionResult OnGet()
         {
+            CurrentCourse = courses.GetCourse(1);
+            Holes = courses.AllHoles(1);
+
             return Page();
         }
 
-        public IActionResult OnPost()
+        public void OnPost()
         {
-            //statistics.UpdateSheet(MockSheet);
-            return Page();
+            statistics.UpdateSheet(MockSheet);
+            //return Page();
         }
     }
 }
