16fd5ee99ea57dde757fb872171f2c65181bf004
Caspar Stanley With <casparstan+creative@gmail.com>
2020-12-17 10:33:05 +0100

    Select Course Page

---
+++ StatsForeGolfers/ProtoWeb/Pages/Courses/SelectCourse.cshtml.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProtoWeb.Interfaces;
+using ProtoWeb.Models;
+
+namespace ProtoWeb.Pages.Courses
+{
+    public class SelectCourseModel : PageModel
+    {
+        private ICourses courses;
+
+        public SelectCourseModel(ICourses repository)
+        {
+            courses = repository;
+        }
+
+        public Dictionary<int, Course> Courses { get; private set; }
+        [BindProperty(SupportsGet = true)] public string FilterCriteria { get; set; }
+
+        public IActionResult OnGet()
+        {
+            Courses = courses.AllCourses();
+            if (!string.IsNullOrEmpty(FilterCriteria))
+            {
+                Courses = courses.FilterCourse(FilterCriteria);
+            }
+            Debug.WriteLine("GetAll OnGet");
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            Courses = courses.AllCourses();
+            if (!string.IsNullOrEmpty(FilterCriteria))
+            {
+                Courses = courses.FilterCourse(FilterCriteria);
+            }
+            Debug.WriteLine("GetAll OnPost");
+
+            return Page();
+        }
+
+        public IActionResult OnPostUserCreate()
+        {
+            if (UserRepository.Instance.Get() != null)
+            {
+                Debug.WriteLine(UserRepository.Instance.Get().Name);
+                Debug.WriteLine(UserRepository.Instance.Get().HomeClub);
+                Debug.WriteLine("GetAllCOurses");
+            }
+
+            return Page();
+        }
+    }
+}
+
